3b9aa4940533781c92a3b8fd25a7d7289cc5675b
Nikita <nickitque.39@gmail.com>
2024-01-22 00:28:11 +0300

    Created models and made first migration

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_descr = models.CharField(max_length=250)
+    price = models.IntegerField()
+
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello world')
+
+
+def item(request):
+    return HttpResponse('Item')
