26d9a4a3fedc3ce2ff9257542caa3a05fe962982
Nikita <nickitque.39@gmail.com>
2024-01-22 12:39:46 +0300

    Added views for food detail

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse('Hello world')
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
+
+    return render(request, 'index.html', context)
 
 
 def item(request):
     return HttpResponse('Item')
+
+
+def detail(request, pk):
+    item = Item.objects.get(pk=pk)
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'detail.html', context)
