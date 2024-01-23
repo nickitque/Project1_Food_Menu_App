949c9a78cf8f1464a1f546a963958e46edf81ee3
Nikita <nickitque.39@gmail.com>
2024-01-23 16:49:38 +0300

    Implemented registration view, template. Created new app users

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
+    path('users/', include('users.urls')),
+
 ]
