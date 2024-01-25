568463b3a5aec830817e62960c8dc32238bf0dad
Nikita <nickitque.39@gmail.com>
2024-01-25 13:46:26 +0300

    added login and logout templates and urlpattenr

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('users/', include('users.urls')),
-
 ]
--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,8 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as authentication_views
+
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    ]
+    path('login/', authentication_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+]
